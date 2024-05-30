929b8241615a70e6eb7fe2bc46f83b6e092552dd
pszponder <14116413+pszponder@users.noreply.github.com>
2024-05-29 21:49:34 -0600

    add log file retention and max size

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -12,6 +12,12 @@ from loguru import logger
 # Load log level from .env file
 load_dotenv(override=True)  # bust env cache by enabling override
 log_level = os.getenv("LOG_LEVEL", "TRACE")
+log_file_max_size = int(
+    os.getenv("LOG_FILE_MAX_SIZE", 1000)
+)  # Max size of log file in bytes
+log_file_retention = int(
+    os.getenv("LOG_FILE_RETENTION", 5)
+)  # Number of log files to retain before deleting oldest
 
 
 # Configure Logger
@@ -27,4 +33,6 @@ logger.add(
     "app.log",
     serialize=True,
     level=log_level,
+    rotation=log_file_max_size,
+    retention=log_file_retention,
 )  # Add another sink, this one will log to a log file called "app.log"
